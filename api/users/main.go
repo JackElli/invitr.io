30d57f87fa6b37d18ebd8ca70736ad092a45f657
Jack Ellis <jackellis100@hotmail.co.uk>
2024-02-14 14:01:12 +0000

    Added first steps into the microservice arch. Basic docker stuff

--- api/users/main.go
+++ api/users/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello users!")
+
+	// Need to make sure that database is set up correctly
+
+	// This microservice holds all of the info about the users
+	// currently in the system
+
+	// it should be able to add, remove, edit users
+	// and should be able to pass info across different
+	// microservices when required.
 }
