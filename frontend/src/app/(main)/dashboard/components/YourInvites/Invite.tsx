ae66142fd08543b06e64d123d4dbc473ed95fda8
Jack Ellis <jackellis100@hotmail.co.uk>
2024-03-27 00:21:27 +0000

    Cool new styling and fixed removed redundant component

--- frontend/src/app/(main)/dashboard/components/YourInvites/Invite.tsx
+++ frontend/src/app/(main)/dashboard/components/YourInvites/Invite.tsx
@@ -1,16 +1,22 @@
 import { getDate, getDateRelative } from "@/app/lib/Date";
 import { Invite } from "../../../../lib/services/InviteService";
+import { USER } from "@/app/(main)/layout";
 
 type Props = {
     _key: string;
     invite: Invite;
 }
 
-const Invite_ = ({ _key, invite }: Props) => {
+const Invite = ({ _key, invite }: Props) => {
     return (
         <div key={_key} className="flex justify-between items-center bg-white p-4 rounded-md border border-gray-200 shadow-sm hover:border-gray-300 cursor-pointer">
             <div>
-                <h1 className="font-bold text-lg">{invite.title}</h1>
+                <div className="flex gap-2 items-center">
+                    <h1 className="font-bold text-lg">{invite.title}</h1>
+                    {invite.organiser == USER &&
+                        <div className='h-3 w-3 bg-yellow-300 rounded-sm' title="You are the organiser of this event"></div>}
+                </div>
+
                 <p><span className="font-bold">{getDate(invite.date)}</span> at {invite.location}</p>
             </div>
 
@@ -19,4 +25,4 @@ const Invite_ = ({ _key, invite }: Props) => {
     )
 }
 
-export default Invite_;
+export default Invite;
--- frontend/src/app/(main)/invite/[id]/components/Person.tsx
+++ frontend/src/app/(main)/invite/[id]/components/Person.tsx
@@ -7,7 +7,11 @@ type Props = {
 const Person = ({ person }: Props) => {
     return (
         <div className="flex items-center justify-between bg-white px-4 py-3 rounded-md border border-gray-200 shadow-sm">
-            <p className="text-lg font-bold">{person.name}</p>
+            <div className="flex gap-2 items-center">
+                <div className="w-6 h-6 rounded-full bg-gray-200"></div>
+                <p className="text-lg font-bold">{person.name}</p>
+            </div>
+
             {person.is_going ?
                 person.is_going == true ? <p className="bg-green-200 px-2 py-1 rounded-xl">Accepted</p> :
                     <p className="bg-red-200 px-2 py-1 rounded-xl">Declined</p>
