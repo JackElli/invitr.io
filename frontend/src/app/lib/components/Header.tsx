79b57a665ef2e6185c7939f84b4a39fbdf7de569
Jack Ellis <jackellis100@hotmail.co.uk>
2024-03-27 21:09:18 +0000

    Minor style changes

--- frontend/src/app/lib/components/Header.tsx
+++ frontend/src/app/lib/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <div className='w-full h-10 bg-gray-200 flex items-center '>
+        <div className='w-full h-10 bg-gray-200 flex items-center sticky top-0'>
             <div className='flex items-center justify-between w-3/4 mx-auto'>
                 <a href='/'>
                     <h1 className="font-bold px-2 py-1 bg-green-200 shadow-sm inline rounded-lg">invitr.io</h1>
