2596ae894c0ee12c5be68261cb0a2774faa72e6f
Jack Ellis <jackellis100@hotmail.co.uk>
2024-03-27 00:21:27 +0000

    Cool new styling and fixed removed redundant component

--- frontend/src/app/(main)/invite/[id]/page.tsx
+++ frontend/src/app/(main)/invite/[id]/page.tsx
@@ -11,13 +11,15 @@ export default async function InvitePage({ params }: { params: { id: string } })
             <div>
                 <div className="border-b border-b-zinc-200 pb-4">
                     <div className="flex justify-between items-center">
-                        <h1 className="font-bold text-5xl">{invite.title}</h1>
+                        <div>
+                            <h1 className="font-bold text-5xl">{invite.title}</h1>
+                            <h1 className="font-semibold text-2xl mt-2">@ {invite.location}</h1>
+                            <h1 className="font-semibold text-xl mt-2">{getDate(invite.date)}</h1>
+                            <p className="text-sm bg-gray-200 inline-block px-2 rounded-sm">{getDateRelative(invite.date)}</p>
+                        </div>
+
                         <Button disabled className="min-w-32 border-red-300 text-red-500">Delete event</Button>
                     </div>
-
-                    <h1 className="font-semibold text-2xl mt-2">@ {invite.location}</h1>
-                    <h1 className="font-semibold text-xl mt-2">{getDate(invite.date)}</h1>
-                    <p className="text-sm bg-gray-200 inline-block px-2 rounded-sm">{getDateRelative(invite.date)}</p>
                 </div>
 
                 <h1 className="text-xl font-bold mt-10">People invited</h1>
