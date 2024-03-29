816e77586048e3b2e7fad2c294ef209dde65c2c2
Jack Ellis <jackellis100@hotmail.co.uk>
2024-03-29 21:33:55 +0000

    Cleaned up some of the code for the UI and also set the organiser to the top of the people list in an event

--- frontend/src/app/(main)/invite/[id]/components/AcceptButton.tsx
+++ frontend/src/app/(main)/invite/[id]/components/AcceptButton.tsx
@@ -5,12 +5,14 @@ import InviteService, { Invite } from "@/app/lib/services/InviteService";
 type Props = {
     invite: Invite;
     // TODO get rid of the username
-    username: string;
+    user: string;
 }
-const AcceptButton = ({ invite, username }: Props) => {
+const AcceptButton = ({ invite, user }: Props) => {
+
     const accept = () => {
-        InviteService.respondToEvent(true, invite.id, username);
+        InviteService.respondToEvent(true, invite.id, user);
     }
+
     return <Button className="bg-green-200" onClick={accept}>Accept</Button>
 }
 
